94a0668d580be01561808874464eb6d5d8fde241
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-06-06 14:55:40 +0200

    Finalize EFAS 2.5min.

--- outputNetcdf.py
+++ outputNetcdf.py
@@ -37,8 +37,8 @@ class OutputNetcdf():
         y_min = np.round(y_max - rows*deltaLat, 2) 
         
         # cell centres coordinates
-        self.longitudes = np.arange(x_min + deltaLon/2., x_max - deltaLon/2., deltaLon)
-        self.latitudes  = np.arange(y_max - deltaLat/2., y_min + deltaLat/2.,-deltaLat)
+        self.longitudes = np.arange(x_min + deltaLon/2., x_max, deltaLon)
+        self.latitudes  = np.arange(y_max - deltaLat/2., y_min,-deltaLat)
 
         #~ # cell centres coordinates
         #~ self.longitudes = np.linspace(x_min + deltaLon/2., x_max - deltaLon/2., cols)
