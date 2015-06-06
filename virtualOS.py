74672e3b4dae904434f44680b448bb31c57a94e2
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-06-06 10:09:02 +0200

    First test.

--- virtualOS.py
+++ virtualOS.py
@@ -624,8 +624,8 @@ def gdalwarpPCR(input,output,cloneOut,tmpDir,isLddMap=False,isNominalMap=False,i
             te+tr+" "+\ 
             '-r '+method+\
             ' -srcnodata -3.4028234663852886e+38 -dstnodata mv '+ \
-              str(tmpDir)+'tmp_inp.tif '+ \
-              str(tmpDir)+'tmp_out.tif'
+            str(tmpDir)+'tmp_inp.tif '+ \
+            str(tmpDir)+'tmp_out.tif'
     cOut,err = subprocess.Popen(co, stdout=subprocess.PIPE,stderr=open(os.devnull),shell=True).communicate()
     # 
     co = 'gdal_translate -of PCRaster '+ \
